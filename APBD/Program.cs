ddf5cc52bd0f79612341e9fe97ced62938c02c17
kegekk <s31084@pjwstk.edu.pl>
2025-06-02 11:50:21 +0200

    commit

--- APBD/Program.cs
+++ APBD/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using APBD.Repositories;
+using APBD.Services;
+using Microsoft.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<ICarRentalRepository, CarRentalRepository>();
+
+builder.Services.AddScoped<IClientService, ClientService>();
+
+var connectionString = builder.Configuration.GetConnectionString("RentalDB");
+builder.Services.AddTransient(_ => new SqlConnection(connectionString));
+
+var app = builder.Build();
